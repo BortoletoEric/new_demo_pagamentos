c7ea8f8bdd7704fcfaa3193cd9e40be8c71c4065
Felipe Biserra <felipe.correa@gertec.com.br>
2023-11-27 16:35:28 -0300

    Tela de sucesso no pagamento Funções de impressão de qrcode e html Navigation da tela de sucesso para o home

---
+++ app/src/main/java/br/com/gertec/autostart/new_demo_pagamentos/commands/Utils.kt
@@ -0,0 +1,62 @@
+package br.com.gertec.autostart.new_demo_pagamentos.commands
+
+import java.text.NumberFormat
+
+class Utils {
+    companion object {
+        fun centsToCurrentString(cents: Double): String? {
+            return NumberFormat.getCurrencyInstance().format(cents.toFloat() / 100)
+        }
+
+        fun getPaymentReceiptHtmlModel(
+            amount: String,
+            applicationType: String,
+            codSale: String, ns: String?
+        ): String? {
+            val head = "<!DOCTYPE html><html>" +
+                    "<head> <meta charset='UTF-8'>                              " +
+                    "<style type='text/css'>                                    " +
+                    "  h2  { margin: 0 0 -12px 0; letter-spacing: -2px;}  " +
+                    "  h4  { margin: 0;}  " +
+                    "  right   { float:right; }     " +
+                    "  left    { float:left; }      " +
+                    "  p    { margin: 0; }                                      " +
+                    "  b    { margin: 0; }                                      " +
+                    "  hr { border-top: 2px solid black; }                      " +
+                    "  body { font-size: 10px; font-family: sans serif;}        " +
+                    "</style>                                                   " +
+                    "</head>"
+            val body = "<body>" +
+                    "<b style=\"font-size:14px\"> Demonstração <right>VIA CLIENTE</right></b>" +
+                    "<hr></hr>" +
+                    "<b> Demonstração - Gertec <right>CNPJ: 03.654.119/0001-76</right> </b>" +
+                    "<b> AV Jabaquara 3060<right> São Paulo </right> </b>" +
+                    "<hr></hr>" +
+                    "<h2> TOTAL: <right> " + amount + " </right> </h2> <br>" +
+                    "<h4> " + applicationType + " <right> " + codSale + " </right> </h4> <br>" +
+                    "<h4> CARTÃO <right> ************* 3388 </right> </h4>" +
+                    "<hr></hr>" +
+                    "<b>25/09/20 - 11h09 <right> (C) </right> </b>" +
+                    "<b>Auto: 73664829 <right> Term: " + ns + "</right></b> </br>" +
+                    "</body>" +
+                    "</html>"
+            return head + body
+        }
+
+        fun getPaymentReceiptQrCode(
+            amount: String,
+            applicationType: String,
+            codSale: String,
+            ns: String?
+        ): String? {
+            return ("Comprovante de pagamento" +
+                    "\nDemonstração - Gertec" +
+                    "\nCNPJ: 03.654.119/0001-76" +
+                    "\nValor da compra: ${amount}" +
+                    "\nPagamento no: ${applicationType}" +
+                    "\nCódigo da venda: ${codSale}" +
+                    "\nTerminal: ${ns}")
+        }
+    }
+
+}
--- app/src/main/java/br/com/gertec/autostart/new_demo_pagamentos/fragments/SucessPayFragment.kt
+++ app/src/main/java/br/com/gertec/autostart/new_demo_pagamentos/fragments/SucessPayFragment.kt
@@ -1,14 +1,24 @@
 package br.com.gertec.autostart.new_demo_pagamentos.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import br.com.gertec.autostart.new_demo_pagamentos.acitivities.MainActivity
+import br.com.gertec.autostart.new_demo_pagamentos.commands.PrinterCommands
+import br.com.gertec.autostart.new_demo_pagamentos.commands.Utils
 import br.com.gertec.autostart.new_demo_pagamentos.databinding.FragmentSucessPayBinding
 import br.com.gertec.autostart.new_demo_pagamentos.databinding.LayoutCardInfoBinding
 import br.com.gertec.autostart.new_demo_pagamentos.databinding.LayoutPaymentInfoBinding
+import br.com.gertec.gedi.GEDI
+import br.com.gertec.gedi.enums.GEDI_INFO_e_ControlNumber
+import br.com.gertec.gedi.exceptions.GediException
+import br.com.gertec.gedi.interfaces.IGEDI
+import kotlin.random.Random
 
 class SucessPayFragment : Fragment() {
 
@@ -17,6 +27,12 @@ class SucessPayFragment : Fragment() {
     private lateinit var cardInfoBinding: LayoutCardInfoBinding
     private lateinit var paymentInfoBinding: LayoutPaymentInfoBinding
     private lateinit var mainActivity: MainActivity
+
+    private lateinit var amount: String
+    private lateinit var pan: String
+    private lateinit var applicationType: String
+    private lateinit var codSale: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -35,14 +51,42 @@ class SucessPayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        GEDI.init(context)
+        amount = mainActivity.mainViewModel.transactionAmount
+        pan = mainActivity.mainViewModel.pan
+        applicationType = mainActivity.mainViewModel.applicationType
+        codSale = getRandomCodSale().toString()
 
-        setupViews()
-
+        binding.displayPaymentInfo.txtAmount.setText(amount)
+        binding.displayCardInfo.txtApplicationType.setText(applicationType)
+        binding.displayCardInfo.txtCodeSaleValue.setText(codSale)
+        binding.btnPrint.setOnClickListener(View.OnClickListener { printComprovante() })
+        binding.btnFinish.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "Venda finalizada com sucesso!", Toast.LENGTH_LONG).show()
+            view.findNavController()
+                .navigate(SucessPayFragmentDirections.actionSucessPayFragmentToAmountFragment())
+        })
     }
 
-    private fun setupViews() {
-        binding.displayPaymentInfo.txtAmount.text = mainActivity.mainViewModel.transactionAmount
-        binding.displayCardInfo.txtFinalCardNumbers.text = mainActivity.mainViewModel.pan
-        binding.displayCardInfo.txtApplicationType.text = mainActivity.mainViewModel.applicationType
+    fun printComprovante() {
+        val printerCommands = context?.let { PrinterCommands(it) }
+        val ns: String? = getNS()
+        val html = Utils.getPaymentReceiptHtmlModel(amount, applicationType, codSale, ns)
+        val qrCode = Utils.getPaymentReceiptQrCode(amount, applicationType, codSale, ns)
+        printerCommands?.printComprovante(html, qrCode)
+    }
+
+    fun getRandomCodSale(): Int {
+        return Random.nextInt(99999)
+    }
+
+    fun getNS(): String? {
+        val mGedi: IGEDI = GEDI.getInstance(context)
+        try {
+            return mGedi.info.ControlNumberGet(GEDI_INFO_e_ControlNumber.SN)
+        } catch (e: GediException) {
+            Log.e("GediException", "Erro ao obter o número de série: ", e)
+            return null
+        }
     }
 }
