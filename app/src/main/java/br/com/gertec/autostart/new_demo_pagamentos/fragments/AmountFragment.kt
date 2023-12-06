bd93dff168bd04a45f87690e305a6b05eb54aed3
david.matano <david.matano@gertec.com.br>
2023-12-06 11:40:58 -0300

    problema na printer OK denovo

--- app/src/main/java/br/com/gertec/autostart/new_demo_pagamentos/fragments/AmountFragment.kt
+++ app/src/main/java/br/com/gertec/autostart/new_demo_pagamentos/fragments/AmountFragment.kt
@@ -65,7 +65,7 @@ class AmountFragment : Fragment() {
             if (hasPhysicalKbd()) {
                 setupPhysicalKbd(view)
             }
-        }, 1000)
+        }, 1500)
 
     }
 
@@ -164,7 +164,7 @@ class AmountFragment : Fragment() {
     private fun checkEvent() {
         CoroutineScope(Dispatchers.IO).launch {
             val resp =
-                mainActivity.mainViewModel.ppCompCommands.checkEvent("0110") //magnético e chip apenas
+                mainActivity.mainViewModel.ppCompCommands.checkEvent("0000") //magnético e chip apenas
             if (!resp.isNullOrEmpty()) {
                 if (resp == "CKE_MC_ERR") {
                     mainActivity.mainViewModel.processCompleted(resp)
